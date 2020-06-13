91255e2f6c4556742d2556615d2897cce5b1383b
Marlos Rodriguez <Marlos2811@hotmail.com>
2020-06-13 14:56:45 -0400

    Add files via upload

---
+++ precache-manifest.395f5c08949aaea74ccfca4b3d09826d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0fda7b62e1efdb161a084c1cfe36286",
+    "url": "/Crud-Redux/index.html"
+  },
+  {
+    "revision": "1b57c6db183e69908038",
+    "url": "/Crud-Redux/static/css/main.6bfb180a.chunk.css"
+  },
+  {
+    "revision": "281005aea8b07c20dee4",
+    "url": "/Crud-Redux/static/js/2.e5968a92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Crud-Redux/static/js/2.e5968a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b57c6db183e69908038",
+    "url": "/Crud-Redux/static/js/main.bf418cd3.chunk.js"
+  },
+  {
+    "revision": "17d75fb4a995954d1deb",
+    "url": "/Crud-Redux/static/js/runtime-main.c35cf0eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2ab6af6e55f4d85b6a0bbad4dc93432a.js"
+  "/Crud-Redux/precache-manifest.395f5c08949aaea74ccfca4b3d09826d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Crud-Redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
