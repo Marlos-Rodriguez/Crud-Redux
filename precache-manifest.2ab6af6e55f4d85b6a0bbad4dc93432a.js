d5b52a9370ca0975d9e0ad27074d22b3caa5feb6
Marlos <Marlos2811@hotmail.com>
2020-06-13 14:10:28 -0400

    Deploy changes

---
+++ precache-manifest.2ab6af6e55f4d85b6a0bbad4dc93432a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2a9cdbe512b96b9cc04134c74b776b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec01be931d955a8902b1",
+    "url": "./static/css/main.dad8f810.chunk.css"
+  },
+  {
+    "revision": "4abc0fbcb657c2f05854",
+    "url": "./static/js/2.d4a919de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d4a919de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec01be931d955a8902b1",
+    "url": "./static/js/main.738de111.chunk.js"
+  },
+  {
+    "revision": "ebc2699bffab78c2d21f",
+    "url": "./static/js/runtime-main.33bbea19.js"
+  }
+]);
--- src/config/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const clienteAxios = axios.create({
-  baseURL: "https://api-crudredux.herokuapp.com/",
-});
-
-export default clienteAxios;
